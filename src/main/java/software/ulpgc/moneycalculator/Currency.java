9c03f7c94fbfe1e79caac5ba4d6936a3f449563f
lauraheerrera <159728136+lauraheerrera@users.noreply.github.com>
2024-11-10 18:57:34 +0000

    Model classes created: Currency, Money, ExchangeRate

---
+++ src/main/java/software/ulpgc/moneycalculator/Currency.java
@@ -0,0 +1,4 @@
+package software.ulpgc.moneycalculator;
+
+public record Currency(String code, String name) {
+}
---
+++ src/main/java/software/ulpgc/moneycalculator/Money.java
@@ -0,0 +1,4 @@
+package software.ulpgc.moneycalculator;
+
+public record Money(double amount, Currency currency) {
+}
