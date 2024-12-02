61e83b3998dc28a46789e5134a598a2212fc9ad7
lauraheerrera <159728136+lauraheerrera@users.noreply.github.com>
2024-12-02 22:30:50 +0000

    Separated MainFrame and data loading logic from Main

--- src/main/java/software/ulpgc/moneycalculator/Main.java
+++ src/main/java/software/ulpgc/moneycalculator/Main.java
@@ -1,5 +1,7 @@
 package software.ulpgc.moneycalculator;
+
 import software.ulpgc.moneycalculator.control.CalculateCommand;
+import software.ulpgc.moneycalculator.control.Command;
 import software.ulpgc.moneycalculator.io.FixerAPI;
 import software.ulpgc.moneycalculator.io.FixerAPIReader;
 import software.ulpgc.moneycalculator.io.currency.CurrencyLoader;
@@ -9,67 +11,25 @@ import software.ulpgc.moneycalculator.io.exchangerate.ExchangeRateLoader;
 import software.ulpgc.moneycalculator.io.exchangerate.FixerExchangeRateAdapter;
 import software.ulpgc.moneycalculator.io.exchangerate.FixerExchangeRateDeserializer;
 import software.ulpgc.moneycalculator.model.Currency;
-import software.ulpgc.moneycalculator.view.SwingCurrencyDialog;
-import software.ulpgc.moneycalculator.view.SwingMoneyDialog;
-import software.ulpgc.moneycalculator.view.SwingMoneyDisplay;
-import javax.swing.*;
-import java.awt.*;
-import java.io.IOException;
 import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        CurrencyLoader loader = new CurrencyLoader(
+    public static void main(String[] args)  {
+        MainFrame mainFrame = new MainFrame();
+        List<Currency> currencies = new CurrencyLoader(
                 new FixerCurrencyAdapter(),
                 new FixerCurrencyDeserializer(),
-                new FixerAPIReader(FixerAPI.FIXER_API_CURRENCIES_URL, FixerAPI.FIXER_API_KEY)
-        );
-        List<Currency> currencies = loader.get();
-
-        ExchangeRateLoader exchangeRateLoader = new ExchangeRateLoader(
-                new FixerExchangeRateAdapter(),
-                new FixerExchangeRateDeserializer(),
-                new FixerAPIReader(FixerAPI.FIXER_API_EXCHANGE_RATE_URL, FixerAPI.FIXER_API_KEY)
-        );
-
-        JFrame frame = new JFrame("Money Calculator");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new FlowLayout());
-
-        JTextField amountField = new JTextField(10);
-        SwingCurrencyDialog fromDialog = new SwingCurrencyDialog("From:");
-        SwingMoneyDialog moneyDialog = new SwingMoneyDialog(amountField, fromDialog);
-        moneyDialog.define(currencies);
-        SwingCurrencyDialog toDialog = new SwingCurrencyDialog("To:");
-        toDialog.define(currencies);
-
-        SwingMoneyDisplay moneyDisplay = new SwingMoneyDisplay();
-
-        CalculateCommand calculateCommand = new CalculateCommand(
-                moneyDialog, toDialog, exchangeRateLoader, moneyDisplay);
-
-        JButton showButton = getButton(calculateCommand);
-
-        frame.add(new JLabel("Enter Amount and Currency:"));
-        frame.add(amountField);
-        frame.add(moneyDialog);
-        frame.add(toDialog);
-        frame.add(showButton);
-        frame.add(moneyDisplay);
-        frame.setSize(600, 300);
-        frame.setVisible(true);
+                new FixerAPIReader(FixerAPI.FIXER_API_CURRENCIES_URL, FixerAPI.FIXER_API_KEY)).get();
+        Command command = new CalculateCommand(
+                mainFrame.moneyDialog().define(currencies),
+                mainFrame.currencyDialog().define(currencies),
+                new ExchangeRateLoader(
+                        new FixerExchangeRateAdapter(),
+                        new FixerExchangeRateDeserializer(),
+                        new FixerAPIReader(FixerAPI.FIXER_API_EXCHANGE_RATE_URL, FixerAPI.FIXER_API_KEY)
+                ),
+                mainFrame.moneyDisplay());
+        mainFrame.add("Calculate money", command);
+        mainFrame.setVisible(true);
     }
-
-    private static JButton getButton(CalculateCommand calculateCommand) {
-        JButton showButton = new JButton("Calculate");
-        showButton.setFont(new Font(Font.SERIF, Font.PLAIN, 18));
-        showButton.addActionListener(e -> {
-            try {
-                calculateCommand.execute();
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-        });
-        return showButton;
-    }
-
 }
---
+++ src/main/java/software/ulpgc/moneycalculator/MainFrame.java
@@ -0,0 +1,101 @@
+package software.ulpgc.moneycalculator;
+
+import software.ulpgc.moneycalculator.control.Command;
+import software.ulpgc.moneycalculator.view.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainFrame extends JFrame {
+    private final JTextField amountField;
+    private final Map<String, Command> commands = new HashMap<>();
+    private MoneyDisplay moneyDisplay;
+    private MoneyDialog moneyDialog;
+    private CurrencyDialog currencyDialog;
+
+    public MainFrame() throws HeadlessException {
+        this.setTitle("Money Calculator");
+        this.setSize(1000, 300);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.amountField = new JTextField();
+        this.add(createMainPanel(), BorderLayout.CENTER);
+        this.add(new JLabel(""), BorderLayout.SOUTH);
+    }
+
+    private Component createMainPanel() {
+        JPanel panel = new JPanel();
+        panel.add(createNorthPanel(), BorderLayout.NORTH);
+        panel.add(createCenterPanel(), BorderLayout.CENTER);
+        panel.add(createSouthPanel(), BorderLayout.SOUTH);
+        return panel;
+    }
+
+    private Component createNorthPanel() {
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Amount:");
+        panel.add(label);
+        amountField.setColumns(12);
+        panel.add(amountField);
+        return panel;
+    }
+
+    private Component createCenterPanel() {
+        JPanel panel = new JPanel();
+        panel.add(createDialogs(), BorderLayout.NORTH);
+        return panel;
+    }
+
+    private Component createSouthPanel() {
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Convert");
+        button.setForeground(Color.WHITE);
+        button.addActionListener(e -> {
+            try {
+                commands.get("Calculate money").execute();
+                amountField.setText("");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        panel.add(button);
+
+        SwingMoneyDisplay display = new SwingMoneyDisplay();
+        this.moneyDisplay = display;
+        panel.add(display);
+        return panel;
+    }
+
+    private Component createDialogs() {
+        JPanel panel = new JPanel();
+        SwingCurrencyDialog fromDialog = new SwingCurrencyDialog("From: ");
+        SwingMoneyDialog moneyDialog = new SwingMoneyDialog(amountField, fromDialog);
+        this.moneyDialog = moneyDialog;
+        panel.add(moneyDialog);
+
+        SwingCurrencyDialog toDialog = new SwingCurrencyDialog("To:");
+        this.currencyDialog = toDialog;
+        panel.add(toDialog);
+
+        return panel;
+    }
+
+    public void add(String name, Command command) {
+        commands.put(name, command);
+    }
+
+    public MoneyDisplay moneyDisplay() {
+        return moneyDisplay;
+    }
+
+    public CurrencyDialog currencyDialog() {
+        return currencyDialog;
+    }
+
+    public MoneyDialog moneyDialog() {
+        return moneyDialog;
+    }
+}
