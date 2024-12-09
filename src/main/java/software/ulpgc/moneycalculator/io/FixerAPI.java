bb302af37895c2b60a0159b9f9345012631dab8f
lauraheerrera <159728136+lauraheerrera@users.noreply.github.com>
2024-12-09 21:57:28 +0000

    Final commit

--- src/main/java/software/ulpgc/moneycalculator/io/FixerAPI.java
+++ src/main/java/software/ulpgc/moneycalculator/io/FixerAPI.java
@@ -1,8 +1,7 @@
 package software.ulpgc.moneycalculator.io;
 
 public class FixerAPI {
-    //public static final String FIXER_API_KEY = "28ebf6583decc179ffbeb4e3c20c8289";
-    public static final String FIXER_API_KEY = "e7a83fdc52a9dac2758d5d8fdc326d4a";
+    public static final String FIXER_API_KEY = "28ebf6583decc179ffbeb4e3c20c8289";
     public static final String FIXER_API_CURRENCIES_URL = "http://data.fixer.io/api/symbols?access_key=";
     public static final String FIXER_API_EXCHANGE_RATE_URL = "https://api.exchangeratesapi.io/v1/latest?access_key=";
 
